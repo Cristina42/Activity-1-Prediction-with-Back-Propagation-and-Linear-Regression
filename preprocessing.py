d352d6714d25ffbb883777952dd336e462d305c9
bellinebos <belline.bos@gmail.com>
2024-12-03 00:29:50 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -19,3 +19,17 @@ numeric_df = df_original.select_dtypes(include=['number'])
 
 # Plot the boxplots
 plt.subplots(figsize=(20, 8))
+
+count = 1
+for i in numeric_df.columns:
+    plt.subplot(2, 10, count)  # Adjust the layout (2 rows, 10 columns)
+    sns.boxplot(data=numeric_df[i])
+    plt.title(i, fontsize=12)
+    count += 1
+
+plt.suptitle('Boxplots of all numeric variables of the dataset', fontsize=16)
+plt.subplots_adjust(top=0.85)  # Adjust the title space to avoid overlap
+plt.tight_layout()  # Ensure that subplots do not overlap
+plt.show()
+
+
