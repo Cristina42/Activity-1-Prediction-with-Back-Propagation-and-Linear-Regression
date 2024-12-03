9310529ecf5d0fc09626d8983976b78ac957e56a
bellinebos <belline.bos@gmail.com>
2024-12-03 12:42:31 +0100

    Update MLR.py

--- MLR.py
+++ MLR.py
@@ -1,1 +1,8 @@
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
 
+# Load the training and testing data from CSV files
+train_data = pd.read_csv('train_data.csv')
+test_data = pd.read_csv('test_data.csv')
